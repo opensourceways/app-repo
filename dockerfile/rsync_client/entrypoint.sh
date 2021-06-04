d8e5d2d93ffbeae4ec0ea9372c47cd6d1853fb14
TommyLike <tommylikehu@gmail.com>
2021-06-04 09:00:18 +0800

    Remove password file parameter

--- dockerfile/rsync_client/entrypoint.sh
+++ dockerfile/rsync_client/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 # shellcheck disable=SC2039
-if [[ ! -f "/etc/rsyncd.secrets" ]]; then
-        echo "password file '/etc/rsyncd.secrets' not existed. exit none zero."
-        exit 1
-fi
-# shellcheck disable=SC2039
 if [[ -z "${SYNC_HOST}" ]]; then
         echo "SYNC_HOST is not defined. exit none zero."
         exit 1
@@ -27,9 +22,9 @@ while true; do
    echo "Starting to sync repos @ $(date)"
    # shellcheck disable=SC2039
    if [[ -z "${ADDITIONAL_PARAMETERS}" ]]; then
-      rsync -av --partial --progress --password-file=/etc/rsyncd.secrets  "${SYNC_HOST}"  "${TARGET_DIR}"
+      rsync -av --partial --progress  "${SYNC_HOST}"  "${TARGET_DIR}"
    else
-      rsync -av --partial --progress ${ADDITIONAL_PARAMETERS} --password-file=/etc/rsyncd.secrets  "${SYNC_HOST}"  "${TARGET_DIR}"
+      rsync -av --partial --progress ${ADDITIONAL_PARAMETERS}  "${SYNC_HOST}"  "${TARGET_DIR}"
    fi
    sleep "${RSYNC_INTERNAL}"
 done
